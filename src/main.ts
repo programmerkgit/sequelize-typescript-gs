5e6e09d2cbabdd832c11d71df1c7e12db9e24e66
keishi kubo <admin@keishis-iMac.local>
2020-07-04 06:40:15 +0900

    delete unused comments

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,3 @@
-/*
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
-*/
-
-
 import { User } from './db/models';
 
 User.findAll().then((users) => {
